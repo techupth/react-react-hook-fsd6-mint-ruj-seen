06083f7b979f5839ce7cc5017016bb1d1adf9380
AtirujRat <136755600+RattRuj@users.noreply.github.com>
2024-06-13 14:08:49 +0700

    feat:create custom hook for fetching post data

---
+++ client/src/hooks/useBlogPosts.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const useBlogPosts = () => {
+  const [posts, setPosts] = useState([]);
+  const [isError, setIsError] = useState(null);
+  const [isLoading, setIsLoading] = useState(null);
+
+  const getPosts = async () => {
+    try {
+      setIsError(false);
+      setIsLoading(true);
+      const results = await axios.get("http://localhost:4000/posts");
+      setPosts(results.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setIsError(true);
+    }
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  return { posts, isError, isLoading };
+};
+
+export default useBlogPosts;
